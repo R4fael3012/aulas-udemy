01fc819e152aa2b834e398d900c7c1f9d3e6aa2d
EBSERH - Rafael Alves Rodrigues <rafael.rodrigues.3@ebserh.gov.br>
2026-04-23 16:58:47 -0300

    first commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'Listagem de usuários')
+
+@section('content')
+    <h1 class="title">{{ $greeting }}</h1>
+    <img src="{{ Vite::asset('resources/images/images.jpg') }}" alt="">
+    @foreach($users as $user)
+        <div class="user-name">{{ $user->name }} ({{ $user->email }})</div>
+    @endforeach
+@endsection
+
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('title', 'Mostrando usuário '  . $user->name)
+
+@section('content')
+    <h1>Mostrar usuário {{ $user->name }}</h1>
+
+    @if($user->id === 2)
+        <div>Sou admin</div>
+    @else
+        <div>Não sou admin</div>
+    @endif
+@endsection
